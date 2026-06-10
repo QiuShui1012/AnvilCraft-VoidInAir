b8352b2e1dcd15b38f46492572d0c2f7bf0ac8f5
QiuShui1012 <qiushui1012@foxmail.com>
2026-06-10 14:57:12 +0800

    fix: correct item tooltip descriptions for Void-related items

--- src/main/java/com/qiushui1012/mod/voidinair/item/tooltip/ItemTooltipManager.java
+++ src/main/java/com/qiushui1012/mod/voidinair/item/tooltip/ItemTooltipManager.java
@@ -35,7 +35,7 @@ public class ItemTooltipManager {
         NORMAL.put(ViaItems.DOLPHIN_AMULET.asItem(), "Gives you Dolphin Grace III and making it less likely for the Guardians to notice");
         NORMAL.put(ViaItems.TRANSCENDED_AMULET.asItem(), "Includes all AnvilCraft's and Void in Air's amulet effects (except Abnormal Amulet)");
 
-        SHIFT.put(ViaItems.TOTEM_OF_VOID.asItem(), "Triggers when falling into the Void.nWhen trigger, grants long enough Slow Falling effect, and teleports to the scaled xz coordinates of the max build height in the Overworld");
+        SHIFT.put(ViaItems.TOTEM_OF_VOID.asItem(), "Triggers when falling into the Void\nWhen trigger, grants long enough Slow Falling effect, and teleports to the scaled xz coordinates of the max build height in the Overworld");
         SHIFT.put(ViaItems.VOID_AMULET.asItem(), "Grants immunity of Void\nWhen falling into the Void, grants long enough Slow Falling effect, and teleports to the scaled xz coordinates of the max build height in the Overworld");
         SHIFT.put(ViaBlocks.VOID_FOUNTAIN.asItem(), "Similar to Mineral Fountain, but can only produce the result of Void Decay\nTrying to generate blocks like Mineral Fountain can lead to some unusual things...");
     }
