31d82648512a321a573e107edaa4a99c41081b29
QiuShui1012 <qiushui1012@foxmail.com>
2026-08-20 13:50:41 +0800

    fix(mixin): replace anno to more trustable one

--- src/main/java/com/qiushui1012/mod/voidinair/mixin/GuardianAttackSelectorMixin.java
+++ src/main/java/com/qiushui1012/mod/voidinair/mixin/GuardianAttackSelectorMixin.java
@@ -1,21 +1,21 @@
 package com.qiushui1012.mod.voidinair.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.qiushui1012.mod.voidinair.init.item.ViaAmulets;
 import dev.dubhe.anvilcraft.api.amulet.AmuletManager;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import javax.annotation.Nullable;
 
 @Mixin(targets = "net.minecraft.world.entity.monster.Guardian$GuardianAttackSelector")
 public class GuardianAttackSelectorMixin {
-    @ModifyExpressionValue(
+    @ModifyConstant(
         method = "test(Lnet/minecraft/world/entity/LivingEntity;)Z",
-        at = @At(value = "CONSTANT", args = "doubleValue=9.0")
+        constant = @Constant(doubleValue = 9.0F)
     )
     private double decreaseRangeForDolphinAmulet(double constant, @Local(argsOnly = true, ordinal = 0) @Nullable LivingEntity target) {
         if (!(target instanceof Player player)) return constant;
